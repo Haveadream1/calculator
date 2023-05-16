ced0c685c1850dc39971c168d42a9dcf46cdf8b7
Haveadream1 <260602david@gmail.com>
2023-05-16 15:12:58 +0200

    Set event listener and query

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
+const plus = document.querySelector('.plus');
+const plusButton = document.addEventListener('click');
+
+const minus = document.querySelector('.minus');
+const minusButton = document.addEventListener('click');
+
+const division = document.querySelector('.division');
+const divisionButton = document.addEventListener('click');
+
+const multiplication = document.querySelector('.multiplication');
+const multiplicationButton = document.addEventListener('click');
+//
+const one = document.querySelector('.one');
+const oneButton = document.addEventListener('click');
+
+const two = document.querySelector('.two');
+const twoButton = document.addEventListener('click');
+
+const three = document.querySelector('.three');
+const threeButton = document.addEventListener('click');
+
+const four = document.querySelector('.four');
+const fourButton = document.addEventListener('click');
+
+const five = document.querySelector('.five');
+const fiveButton = document.addEventListener('click');
+
+const six = document.querySelector('.six');
+const sixButton = document.addEventListener('click');
+
+const seven = document.querySelector('.seven');
+const sevenButton = document.addEventListener('click');
+
+const eight = document.querySelector('.eight');
+const eightButton = document.addEventListener('click');
+
+const nine = document.querySelector('.nine');
+const nineButton = document.addEventListener('click');
+
+
 /*  Calculator with basic functions
     4 operators + - / * 
     the operations should take 3 variables, values and operator
-    
 */
